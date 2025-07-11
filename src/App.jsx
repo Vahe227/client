89a37b9f26715c73ba2ae29f47dcac5cc01fa0db
eminisrayelyan <israyelyan.emin@gmail.com>
2025-07-11 16:15:27 +0400

    for client repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import client from 'socket.io-client'
 const socket = client.io('http://localhost:3000');
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [allUsers, setAllUsers] = useState({});
+  const enemiesCounts = Object.values(allUsers);
+
+  useEffect(() => {
+    socket.on('update-count', (serverAllUsers) => {
+      console.log('update-count', socket.id , serverAllUsers);
+      const currentUserCount = serverAllUsers[socket.id];
+      setCount(currentUserCount);
+      delete serverAllUsers[socket.id];
+      setAllUsers(serverAllUsers);
+    });
+    return () => {
+      socket.off('update-count');
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    socket.emit('increase-count');
+  }
 
   return (
-    <>
+    <>    
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleButtonClick}>
+          Click To Increase Count
         </button>
+        <p>Your Count Is {count}</p>
+        {enemiesCounts.map((count,index) => (
+          <p key={index}>Enemy Count Is {count}</p>
+        ))}
       </div>
     </>
   )
