ed4c46a65ab18e9b309ef47c4c5b7172929a3d7f
Vahe227 <vxachatryan650@gmail.com>
2025-07-13 14:55:37 +0400

    for client repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,68 @@
-// Imcorrect code or with errors code
-import { useEffect, useState } from 'react'
+// Importing Packages 
+
+import { useEffect, useState } from "react"
 import './App.css'
 import client from 'socket.io-client'
+
+// React.js code (front-end) is connected to Node.js code (back-end or Server code).
+
 const socket = client.io('http://localhost:3000');
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [allUsers, setAllUsers] = useState({});
+
+  // UseState is used for the number of changes, so that when Clint clicks on the button, the count is increased by 1.
+
+  const [count,setCount] = useState(0);
+  const [allUsers,setAllUsers] = useState({});
+
+  // The enemiesCounts variable is created because we need to get the enemy counts as an array.
+
   const enemiesCounts = Object.values(allUsers);
 
+  // Using useEffect and inside it useState
+
   useEffect(() => {
+
+    // responding to the "update count" event
+
     socket.on('update-count', (serverAllUsers) => {
-      console.log('update-count', socket.id , serverAllUsers);
+
+// In the console, we print the line 'update-count' and the client IDs.
+
+      console.log('update-count', socket.id, serverAllUsers);
+      //  A currentUserCount variable is created, which contains the client count.
       const currentUserCount = serverAllUsers[socket.id];
+      // We set that count in the setCount function with useState
       setCount(currentUserCount);
+      // All customer counts are being deleted. 
       delete serverAllUsers[socket.id];
       setAllUsers(serverAllUsers);
     });
     return () => {
+      // turn off 'update-count' event 
       socket.off('update-count');
     };
   }, []);
 
+  // Creating a handleButtonClick variable that has an arrow function that increments the count variable by 1 when the customer clicks the button.
   const handleButtonClick = () => {
     socket.emit('increase-count');
-  }
+  };
+  
+  // App function returns client counts
 
   return (
-    <>    
-      <div className="card">
-        <button onClick={handleButtonClick}>
-          Click To Increase Count
-        </button>
-        <p>Your Count Is {count}</p>
-        {enemiesCounts.map((count,index) => (
-          <p key={index}>Enemy Count Is {count}</p>
-        ))}
-      </div>
-    </>
+    // By creating a div whose className is "card", and inside the div we have a button, for which when the customer clicks on that button, the sum of their counters increases by 1.
+    <div className="card">
+      <button onClick={handleButtonClick}>
+        Click To Increase Count
+      </button>
+      <p>Your Count Is {count}</p>
+      {enemiesCounts.map((count,index) => (
+        <p key={index}>Enemy Count Is {count}</p>
+      ))}
+    </div>
   )
-}
+};
 
-export default App
-
-
-// correct code 
-
-// import { useState, useEffect } from 'react';
-// import './App.css';
-// import client from 'socket.io-client';
-
-// const socket = client.io('http://localhost:3000');
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [allUsers, setAllUsers] = useState({});
-//   const enemiesCounts = Object.values(allUsers);
-
-//   useEffect(() => {
-//     socket.on('update-count', (serverAllUsers) => {
-//       console.log('update-count', socket.id, serverAllUsers);
-//       const currentUserCount = serverAllUsers[socket.id];
-//       setCount(currentUserCount);
-//       delete serverAllUsers[socket.id];
-//       setAllUsers(serverAllUsers);
-//     });
-
-//     return () => {
-//       socket.off('update-count');
-//     };
-//   }, []);
-
-//   const handleButtonClick = () => {
-//     socket.emit('increase-count');
-//   }
-
-//   return (
-//     <div className="card">
-//       <button onClick={handleButtonClick}>
-//         Click to increase count
-//       </button>
-//       <p>Your count is {count}</p>
-//       {enemiesCounts.map((count, index) => (
-//         <p key={index}>Enemy count is {count}</p>
-//       ))}
-//     </div>
-//   )
-// }
-
-// export default App;
-
+export default App;
