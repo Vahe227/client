67291f946de4e940fe282eba18f07b8e48e52e98
1ErikArakelyan1 <erarakelyan10@gmail.com>
2025-07-16 15:11:59 +0400

    for client repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Importing Packages 
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import client from 'socket.io-client'
 
 // React.js code (front-end) is connected to Node.js code (back-end or Server code).
@@ -17,11 +17,15 @@ function App() {
   // The enemiesCounts variable is created because we need to get the enemy counts as an array.
 
   const enemiesCounts = Object.values(allUsers);
+  const [width,setWidth] = useState(30);
+  const trackRef = useRef(null);
+
+  const pxPerTile = width / 30;
 
   // Using useEffect and inside it useState
 
   useEffect(() => {
-
+    
     // responding to the "update count" event
 
     socket.on('update-count', (serverAllUsers) => {
@@ -53,6 +57,16 @@ function App() {
     };
   }, []);
 
+// Createing second useEffect for ResizeObserver 
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(([entry]) => {
+      setWidth(entry.contentRect.width);
+    });
+    if(trackRef.current) resizeObserver.observe(trackRef.current);
+    return () => resizeObserver.disconnect();
+  }, []);
+
   // Creating a handleButtonClick variable that has an arrow function that increments the count variable by 1 when the customer clicks the button.
   const handleButtonClick = () => {
     socket.emit('increase-count');
@@ -62,38 +76,42 @@ function App() {
   console.log(enemiesCounts)
   return (
     // By creating a div whose className is "card", and inside the div we have a button, for which when the customer clicks on that button, the sum of their counters increases by 1.
-    <>
-      <div 
-          style={{
-            width: '100px',
-            height: '100px',
-            position: 'relative',
-            left: `${count * 10}px`,
-            marginBottom: '10px',
-            backgroundColor: 'pink'
-          }} 
-        />
-      {enemiesCounts.map((count,index) => {
-        return (
-          <div 
-            style={{  
+    <div className="flex flex-col gap-4">
+      <div style={{ width: '90vw' , maxWidth: 900 }} ref={trackRef}>
+        <div 
+            style={{
               width: '100px',
               height: '100px',
               position: 'relative',
-              left: `${count * 10}px`,
+              left: `calc(${(count / 30) * 100}%)`,
               marginBottom: '10px',
-              backgroundColor: 'pink'
+              backgroundColor: 'pink',
+              transition: `left ${pxPerTile * 3}ms linear`,
             }} 
-            key={index}
-        />
-        )
-      })}
+          />
+        {enemiesCounts.map((count,index) => {
+          return (
+            <div 
+              style={{  
+                width: '100px',
+                height: '100px',
+                position: 'relative',
+                left: `calc(${(count / 30) * 100}%)`,
+                marginBottom: '10px',
+                backgroundColor: 'pink',
+                transition: `left ${pxPerTile * 3}ms linear`, 
+              }} 
+              key={index}
+          />
+          )
+        })}
+      </div>
       <div className="card">
         <button onClick={handleButtonClick}>
           Click To Increase Count
         </button>
       </div>
-    </>
+    </div>
   )
 };
 
